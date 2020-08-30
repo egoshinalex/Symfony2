6238eb8ebdf022cf3a3a749fd8272210f5d5a0e7
egoshinalex <avers-pravo@mail.ru>
2020-08-30 15:36:55 +0300

    first

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Page;
 use App\services\TestService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -16,8 +19,79 @@ class MainController extends AbstractController
      */
     public function index(TestService $service)
     {
-        $tmp = $service->convert(1000);
+        $tmp = $service->convert(10000);
         return $this->render('main/index.html.twig'
         , ['key' => $tmp]);
     }
+    /**
+     * @Route("/add-page", name="addPage")
+     */
+    public function addPage(EntityManagerInterface $em)
+    {
+       $page = new Page();
+       $page->setContent('Это контент2');
+       $page->setTitle('Это заголовок2');
+       $page->setPublish('true');
+
+       $em->persist($page);
+       $em->flush();
+
+       return new Response('<html><body>Объект добавлен</body></html>');
+
+
+    }
+    /**
+     * @Route("/show-page/{id}", name="showPage")
+     */
+    //public function showPage($id, EntityManagerInterface $em)
+    public function showPage(Page $page)
+    {
+//        $repository = $em->getRepository(Page::class);
+//        $page = $repository->findOneBy(['id'=>$id]);
+//        if(!$page){
+//            throw $this->createNotFoundException(sprintf('Такой страницы не найдено с id = "%s"', $id));
+//
+//        }
+        return $this->render('page.html.twig'
+            , ['page' => $page]);
+       //dd($page);
+
+      //  return new Response('<html><body>Объект добавлен</body></html>');
+
+    }
+    /**
+     * @Route("/edit-page/{id}", name="editPage")
+     */
+    public function editPage(Page $page, EntityManagerInterface $em)
+    {
+          $page->setTitle('Обновленный заголовок');
+          $page->setPublish(false);
+          $em->flush();
+
+        return new Response('<html><body>Объект обновлен</body></html>');
+    }
+
+    /**
+     * @Route("/delete-page/{id}", name="deletePage")
+     */
+    public function deletePage(Page $page, EntityManagerInterface $em)
+    {
+        $em->remove($page);
+        $em->flush();
+
+        return new Response('<html><body>Объект удален</body></html>');
+    }
+    /**
+     * @Route("/index-page", name="indexPage")
+     */
+    public function indexPage(EntityManagerInterface $em)
+    {
+       // $pages = $em->getRepository(Page::class)->findBy([]);
+        $pages = $em->getRepository(Page::class)->findBy(['id' => '10']);
+        dd($pages);
+
+    }
+
+
+
 }
---
+++ src/Entity/Page.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PageRepository::class)
+ */
+class Page
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $publish;
+
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPublish()
+    {
+        return $this->publish;
+    }
+
+    /**
+     * @param mixed $publish
+     */
+    public function setPublish($publish): void
+    {
+        $this->publish = $publish;
+    }
+}
